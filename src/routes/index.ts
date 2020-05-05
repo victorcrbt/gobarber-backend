154e49755f96afb00415a33913bd9bc6e5160022
Victor <madivcb@gmail.com>
2020-05-04 22:41:58 -0300

    setup vscode debug

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,12 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (req, res) => res.send('Hello, World!'));
+routes.post('/users', (req, res) => {
+  const { name, email } = req.body;
+
+  const user = { name, email };
+
+  return res.status(201).json(user);
+});
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
+
 app.use(routes);
 
 const PORT = 3333;
