55df37c7ad1259c30df798345c7e03da00495848
Victor <madivcb@gmail.com>
2020-05-04 23:10:08 -0300

    add time already taken validation

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,33 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
 
 const routes = Router();
 
-const appointments = [];
+const appointments: Appointment[] = [];
 
 routes.post('/', (req, res) => {
   const { provider, date } = req.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppointmentInSameHour = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date)
+  );
+
+  if (findAppointmentInSameHour) {
+    return res.status(403).json({ error: 'This time is already taken.' });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
