9f2d72e11753d616a23d47ced0a67d2dbe314252
Victor <madivcb@gmail.com>
2020-05-04 23:34:51 -0300

    add method to find all appointments

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -17,6 +17,10 @@ class AppointmentsRepository {
     return appointment;
   }
 
+  public findAll(): Appointment[] {
+    return this.appointments;
+  }
+
   public findByDate(date: Date): Appointment | null {
     const findAppointment = this.appointments.find(appointment =>
       isEqual(date, appointment.date)
