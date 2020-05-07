46e943f0fb978d5bdb9535be97f2bd87026ec160
Victor Batalha <madivcb@gmail.com>
2020-05-06 23:34:21 -0300

    create users routes

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+
+import CreateUserService from '../services/CreateUserService';
+
+const routes = Router();
+
+routes.post('/', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.run({ name, email, password });
+
+    return res.status(201).json(user);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
+
+export default routes;
