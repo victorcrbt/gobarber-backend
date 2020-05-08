e337cae56c865c9ea120cbf2d1a4ea012b3e6102
Victor <madivcb@gmail.com>
2020-05-07 22:45:14 -0300

    delete password from return

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -12,6 +12,8 @@ routes.post('/', async (req, res) => {
 
     const user = await createUser.run({ name, email, password });
 
+    delete user.password;
+
     return res.status(201).json(user);
   } catch (error) {
     return res.status(400).json({ error: error.message });
