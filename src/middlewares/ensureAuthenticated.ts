8adffa177e41ee60847e73332fc42f61c4f451e6
Victor <madivcb@gmail.com>
2020-05-07 23:45:02 -0300

    created authentication middleware

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import authConfig from '../config/auth';
+
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default async function ensureAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error('Missing token.');
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = verify(token, authConfig.jwt.secret) as TokenPayload;
+
+    req.user = {
+      id: decoded.sub,
+    };
+
+    return next();
+  } catch (error) {
+    throw new Error('Invalid token.');
+  }
+}
