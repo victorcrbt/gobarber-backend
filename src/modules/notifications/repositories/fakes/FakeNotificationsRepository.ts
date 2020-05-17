d63092f3cf0348b373105bb31b562c9fa8ffd599
Victor <madivcb@gmail.com>
2020-05-17 13:52:32 -0300

    fixed fake notifications repository

--- src/modules/notifications/repositories/fakes/FakeNotificationsRepository.ts
+++ src/modules/notifications/repositories/fakes/FakeNotificationsRepository.ts
@@ -1,3 +1,5 @@
+import { ObjectID } from 'mongodb';
+
 import Notification from '@modules/notifications/infra/typeorm/schemas/Notification';
 
 import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
@@ -18,12 +20,19 @@ export default class FakeNotificationsRepository
     const notification = new Notification();
 
     Object.assign(notification, {
+      id: new ObjectID(),
       content,
       recipient_id,
+      created_at: new Date(Date.now()),
+      updated_at: new Date(Date.now()),
     });
 
     this.notifications.push(notification);
 
     return notification;
+  }
+
+  public async findAll(): Promise<Notification[]> {
+    return this.notifications;
   }
 }
