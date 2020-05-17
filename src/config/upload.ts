d1b16e8061f5d016c8fb87198189570af66226a1
Victor <madivcb@gmail.com>
2020-05-17 18:54:07 -0300

    changed upload config file

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -4,16 +4,45 @@ import { resolve } from 'path';
 
 const tempFolder = resolve(__dirname, '..', '..', 'temp');
 
+interface IUploadConfig {
+  driver: 'disk' | 's3';
+  tempFolder: string;
+  uploadsFolder: string;
+
+  multer: {
+    storage: multer.StorageEngine;
+  };
+
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
+
 export default {
+  driver: process.env.APP_STORAGE_DRIVER,
+
   tempFolder,
   uploadsFolder: resolve(tempFolder, 'uploads'),
-  storage: multer.diskStorage({
-    destination: tempFolder,
-    filename(req, file, cb) {
-      const fileHash = crypto.randomBytes(10).toString('HEX');
-      const fileName = `${fileHash}-${file.originalname}`;
 
-      return cb(null, fileName);
+  multer: {
+    storage: multer.diskStorage({
+      destination: tempFolder,
+      filename(req, file, cb) {
+        const fileHash = crypto.randomBytes(10).toString('HEX');
+        const fileName = `${fileHash}-${file.originalname}`;
+
+        return cb(null, fileName);
+      },
+    }),
+  },
+
+  config: {
+    disk: {},
+    aws: {
+      bucket: process.env.APP_BUCKET_NAME,
     },
-  }),
-};
+  },
+} as IUploadConfig;
