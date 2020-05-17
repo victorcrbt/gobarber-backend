f423dbc08731d0874ba18a5e29928d1dc5ff5200
Victor <madivcb@gmail.com>
2020-05-17 01:22:18 -0300

    created routes to list provider month and availability

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -3,12 +3,25 @@ import { Router } from 'express';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController';
+import ProviderMonthAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderDayAvailabilityController';
 
 const routes = Router();
 const providersController = new ProvidersController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 
 routes.use(ensureAuthenticated);
 
 routes.get('/', providersController.index);
+
+routes.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityController.index
+);
+routes.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.index
+);
 
 export default routes;
