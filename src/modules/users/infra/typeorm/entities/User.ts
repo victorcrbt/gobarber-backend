a2b38b364545f78e7a190af6c95f5ff2456b16e9
Victor <madivcb@gmail.com>
2020-05-17 18:54:53 -0300

    set dynamic storage provider based on env varabile

--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -7,6 +7,8 @@ import {
 } from 'typeorm';
 import { Exclude, Expose } from 'class-transformer';
 
+import uploadConfig from '@config/upload';
+
 @Entity('users')
 class User {
   @PrimaryGeneratedColumn('uuid')
@@ -33,9 +35,16 @@ class User {
 
   @Expose({ name: 'avatar_url' })
   getAvatarUrl(): string | null {
-    return this.avatar
-      ? `${process.env.APP_API_URL}/files/${this.avatar}`
-      : null;
+    if (!this.avatar) return null;
+
+    switch (uploadConfig.driver) {
+      case 's3': {
+        return `https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${this.avatar}`;
+      }
+      default: {
+        return `${process.env.APP_API_URL}/files/${this.avatar}`;
+      }
+    }
   }
 }
 
